9bb3ec08b4c6a438800298a667c65b725ec17cee
QSP <QSP@DESKTOP-EVK9GMC>
2021-03-14 11:25:57 +0530

    DataDriven

---
+++ src/actions/Snippet.java
@@ -0,0 +1,8 @@
+package actions;
+
+public class Snippet {
+	System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+}
+
